beb0f237813799f7df3d2202b9388cfae206cdaa
flohh-py <jbmflohh@gmail.com>
2022-01-04 21:42:29 -0300

    added main app and navbar example

---
+++ mysite/main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.http import JsonResponse
+from .models import Main
+
+
+class MainView(View):
+    def get(self, request):
+        titles = list(Main.objects.values())
+
+        return render(request, 'main/main.html')
