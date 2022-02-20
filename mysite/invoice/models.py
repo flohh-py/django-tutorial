a62d38a46d300641fc9fe72bacea8b3575a0df04
flohh-py <jbmflohh@gmail.com>
2022-02-20 10:00:48 -0300

    fix on save invoice, move fields on template

--- mysite/invoice/models.py
+++ mysite/invoice/models.py
@@ -27,10 +27,12 @@ class Invoice(models.Model):
         return self.code
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        if self.type == 'purchase':
-            self.code = NS.get_series(serie='PINV')
-        if self.type == 'sell':
-            self.code = NS.get_series(serie='SINV')
+        if not self.id:
+            print("CREATE INOVICE")
+            if self.type == 'purchase':
+                self.code = NS.get_series(serie='PINV')
+            if self.type == 'sell':
+                self.code = NS.get_series(serie='SINV')
         return super().save(force_insert, force_update, using, update_fields)
 
     class Meta:
