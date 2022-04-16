147568ad3b3592fad60573a79a412727fafed1b8
flohh-py <jbmflohh@gmail.com>
2022-04-16 11:48:18 -0400

    working on user custom user models

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import NamingSeries
+from .models import NamingSeries, MainUser
 
 # Register your models here.
 admin.site.register(NamingSeries)
+admin.site.register(MainUser)
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 NAMING_TYPE = [
     ('default', 'Default'),
@@ -26,4 +27,34 @@ class NamingSeries(models.Model):
             s.number = int(s.number) + 1
             s.save()
             return str(serie) +' - ' + str(s.number).zfill(s.fill)
+
+
+class MainAccontManager(BaseUserManager):
+    def create_user(self, email, user_name, password, **other_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, user_name, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_manager', True)
+        other_fields.setdefault('is_active', True)
+        
+        return self.create_user(email, user_name, password, is_superuser=True, **other_fields)
+
+
+class MainUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    user_name = models.CharField(max_length=50, unique=True)
+
+    is_manager = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    objects = MainAccontManager()
+
+    USERNAME_FIELD = 'user_name'
+    REQUIRED_FIELDS = ['email']
 
